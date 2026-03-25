2fb1a660765ef79f674abbd643ce19b73be88561
Lucas DIEDRICHS <diedrichs.l@outlook.com>
2026-03-25 16:39:26 +0100

    Add files via upload

---
+++ SQL-Contraintes-Metier-TripPlanner.sql
@@ -0,0 +1,14 @@
+﻿ALTER TABLE Vote
+ADD CONSTRAINT UQ_Vote_Utilisateur_Proposition
+UNIQUE (UtilsateurId, PropositionId);
+
+
+ALTER TABLE Participation
+ADD CONSTRAINT UQ_Participation_Utilisateur_Voyage
+UNIQUE (UtilisateurId, VoyageId);
+
+
+
+CREATE UNIQUE INDEX UQ_Proposition_Voyage_Type_Retenue
+ON Proposition (VoyageId, TypeProposition)
+WHERE EstRetenue = 1;
